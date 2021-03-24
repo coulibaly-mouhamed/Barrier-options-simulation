ce5b7cf0b09460392af6b2cd573306c665dacc03
coulibaly-mouhamed <63076571+coulibaly-mouhamed@users.noreply.github.com>
2021-03-24 11:45:58 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <list>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <math.h>
+#include <chrono>
+#include <ctime>
+#include<random>
+#include"options.hpp"
---
+++ options.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <list>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <math.h>
+#include <chrono>
+#include <ctime>
+#include<random>
+
+enum variable {uniform,gaussian};
+class random_variable
+{
+
+};
